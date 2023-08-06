b7470fbf0df9e1db93d0bbb8ee2b466b37cad860
Ada <the.nostra.tymus@gmail.com>
2023-08-05 23:51:35 -0400

    updated the run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-cd yolov5
-python train.py --img 1500 --epochs 60 --data ../first-generation.yaml  --batch-size -1
-python train.py --img 1500 --epochs 60 --data ../threshold-transmission.yaml  --batch-size -1
-
+cd yolov5 || exit
+python train.py --img 1500 --epochs 60 --data ../non-threshold-subsignal.yaml  --batch-size -1
+python train.py --img 1500 --epochs 60 --data ../non-threshold-transmission.yaml  --batch-size -1
+python train.py --img 1500 --epochs 60 --data ../threshold-subsignal.yaml  --batch-size -1
+python train.py --img 1500 --epochs 60 --data ../first-generation-v2.yaml  --batch-size -1
+python train.py --img 1500 --epochs 60 --data ../threshold-transmission-v2.yaml  --batch-size -1
